2f48835f1976f0b0274d7f1da7e4450be2310435
Emre Ozsahin <emreozsahin@pal-nat184-141-099.itap.purdue.edu>
2015-02-20 14:08:17 -0500

    first functioning version

--- algo.c
+++ algo.c
@@ -84,14 +84,14 @@ void placeShips(Point m[MATRIX_SIZE][MATRIX_SIZE]){
     }
 }
 
-Point *insertionSort(Point m[MATRIX_SIZE][MATRIX_SIZE],Point arr[MATRIX_SIZE*MATRIX_SIZE/2]){
+Point *insertionSortProbs(Point m[MATRIX_SIZE][MATRIX_SIZE],Point arr[MATRIX_SIZE*MATRIX_SIZE]){
     int row,col;
     int count = 0;
     //create an array of points 
 
     for(row = 0; row < MATRIX_SIZE; row++ ){
         for(col = 0; col < MATRIX_SIZE; col++){
-            if(m[row][col].prob){
+            if(m[row][col].prob > 0 && m[row][col].firedUpon == FALSE){
                 arr[count] = m[row][col];
                 count++;
             }
@@ -115,6 +115,25 @@ Point *insertionSort(Point m[MATRIX_SIZE][MATRIX_SIZE],Point arr[MATRIX_SIZE*MAT
 
 
    return arr;
+}
+
+void fire_insertionSort(Point *arr,int size){
+
+    int  i,j;
+    Point temp;
+
+    
+
+    for (i = 1; i < size; i++) {
+      temp = arr[i];
+      j = i - 1;
+      while ((temp.prob > arr[j].prob) && (j >= 0)) {
+         arr[j + 1] = arr[j];
+         j = j - 1;
+      }
+      arr[j + 1] = temp;
+   }
+   return;
 }
 
 int checkShips(Point m[MATRIX_SIZE][MATRIX_SIZE], int size, int row, int col){
@@ -194,16 +213,21 @@ void calcProbability(Point m[MATRIX_SIZE][MATRIX_SIZE],int *remaining_ship_freq,
     int ship_placement_3 = checkShips(m,3,row,col);
     int ship_placement_2 = checkShips(m,2,row,col);
 
-    int overall_prob = remaining_ship_freq[0]*ship_placement_5 +
-                        remaining_ship_freq[1]*ship_placement_4 +
-                        remaining_ship_freq[2]*ship_placement_3 +
-                        remaining_ship_freq[3]*ship_placement_2;
+    int overall_prob = remaining_ship_freq[3]*ship_placement_5 +
+                        remaining_ship_freq[2]*ship_placement_4 +
+                        remaining_ship_freq[1]*ship_placement_3 +
+                        remaining_ship_freq[0]*ship_placement_2;
 
     m[row][col].prob = overall_prob;
 }
 
-Point findBestShot(Point m[MATRIX_SIZE][MATRIX_SIZE], int *remaining_ship_freq){
+void fire_calcProbability(Point m[MATRIX_SIZE][MATRIX_SIZE], int *remaining_ship_freq,int row, int col){
+
+}
+
+Point hunt_findShot(Point m[MATRIX_SIZE][MATRIX_SIZE], int *remaining_ship_freq){
     int row,col;
+
 
     //calculate probabilities of each checkerboard grid
     for(row = 0; row < MATRIX_SIZE;row++){
@@ -217,21 +241,152 @@ Point findBestShot(Point m[MATRIX_SIZE][MATRIX_SIZE], int *remaining_ship_freq){
     }
 
     //sort the possible options
+    int size = 0;
 
     printf("\nsorted probs:\n");
-    Point arr[MATRIX_SIZE*MATRIX_SIZE] = {0};
-    insertionSort(m,arr);
-    for(row = 0; row < MATRIX_SIZE*MATRIX_SIZE; row++){
+    Point arr[MATRIX_SIZE*MATRIX_SIZE/2] = {0};
+    insertionSortProbs(m,arr);
+    for(row = 0; row < MATRIX_SIZE*MATRIX_SIZE/2; row++){
         printf("point(%d,%d): prob: %d\n",arr[row].x,arr[row].y,arr[row].prob);
+        if(arr[row].prob > 0)
+            size++;
     }
 
-    srand(time(NULL));
+    // time_t seconds;
+    // time(&seconds);
+    // srand((unsigned int) seconds);
+
     int lowerBound = 0;
     int upperBound = PRECISION;
+    if(size < PRECISION)
+        upperBound = size;
     int rndValue = lowerBound + rand() % (upperBound - lowerBound);
-    printf("\n**selected point is (%d,%d) with prob %d\n",arr[rndValue].x,arr[rndValue].y,arr[rndValue].prob);
+    printf("\n**rnd value: %d ,selected point is (%d,%d) with prob %d\n",rndValue,arr[rndValue].x,arr[rndValue].y,arr[rndValue].prob);
 
-    return arr[rndValue];
+    return arr[rndValue]; 
+    /*printf("\n**selected point is (%d,%d) with prob %d\n",arr[0].x,arr[0].y,arr[0].prob);
+
+    return arr[0];*/
+
+}
+
+
+
+Point fire_findShot(Point m[MATRIX_SIZE][MATRIX_SIZE], int *remaining_ship_freq, Point target_point, int fire_dir){
+    Point arr[ARR_FIRE_SIZE] = {0};
+    Point idle_point;
+    idle_point.prob = 0;
+    idle_point.x = 0;
+    idle_point.y = 0;
+    arr[0] = idle_point;
+    int ind = 0;
+    int row;
+
+    //DIFFERENT PROB CALCULATION NEEDED FOR FIRE 
+
+    if(!m[target_point.x][target_point.y].firedUpon){
+        printf("Error: Given target_point is null\n");
+        return arr[0];
+    } else {
+        int row = target_point.x;
+        int col = target_point.y;
+
+        if(fire_dir == FIRE_IDLE){
+            //check probability for north and south
+            
+            //FIRE_COL  
+            if(row - 1 >= 0 ){
+                //check north
+                if(m[row-1][col].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row-1,col);
+                    arr[ind] = m[row-1][col];
+                    ind++;
+                }
+            }
+
+            if(row + 1 < MATRIX_SIZE){
+                //check south
+                if(m[row+1][col].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row+1,col);
+                    arr[ind] = m[row+1][col];
+                    ind++;
+                }
+            }
+
+            //FIRE_ROW
+            if(col - 1 >= 0 ){
+                //check west
+                if(m[row][col-1].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row,col-1);
+                    arr[ind] = m[row][col-1];
+                    ind++;
+                }
+            }
+
+            if(col + 1 < MATRIX_SIZE){
+                //check east
+                if(m[row][col+1].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row,col+1);
+                    arr[ind] = m[row][col+1];
+                    ind++;
+                }
+            }
+        } else if(fire_dir == FIRE_ROW){
+            
+            //FIRE_ROW
+            if(col - 1 >= 0 ){
+                //check west
+                if(m[row][col-1].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row,col-1);
+                    arr[ind] = m[row][col-1];
+                    ind++;
+                }
+            }
+
+            if(col + 1 < MATRIX_SIZE){
+                //check east
+                if(m[row][col+1].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row,col+1);
+                    arr[ind] = m[row][col+1];
+                    ind++;
+                }
+            }
+
+        } else if(fire_dir == FIRE_COL){
+
+            //FIRE_COL  
+            if(row - 1 >= 0 ){
+                //check north
+                if(m[row-1][col].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row-1,col);
+                    arr[ind] = m[row-1][col];
+                    ind++;
+                }
+            }
+
+            if(row + 1 < MATRIX_SIZE){
+                //check south
+                if(m[row+1][col].firedUpon == FALSE){
+                    calcProbability(m,remaining_ship_freq,row+1,col);
+                    arr[ind] = m[row+1][col];
+                    ind++;
+                }
+            }
+             
+        }
+
+        
+
+
+        //sort the arr and pick rand point given precision
+        fire_insertionSort(&arr[0],ind);
+        for(row = 0; row < ARR_FIRE_SIZE; row++){
+            printf("point(%d,%d): prob: %d\n",arr[row].x,arr[row].y,arr[row].prob);
+        }
+
+        return arr[0];
+
+    }
 }
 
 int fire(Point single_point, Point m[MATRIX_SIZE][MATRIX_SIZE]){
@@ -249,14 +404,46 @@ int fire(Point single_point, Point m[MATRIX_SIZE][MATRIX_SIZE]){
 
 
 
-
-
-void displayProbs(Point m[MATRIX_SIZE][MATRIX_SIZE]){
+void displayMatrix(Point m[MATRIX_SIZE][MATRIX_SIZE], int opt){
     int i,j;
-    printf("\n\n Displaying Probabilities: \n\n");
+    
     for(i = 0; i < MATRIX_SIZE; i++){
         for(j = 0; j < MATRIX_SIZE; j++){
-            printf("%2d  ",m[i][j].prob);
+
+            switch(opt){
+
+                case PRINT_SHIPS:
+                    if(!i && !j)
+                        printf("\n\n Displaying Ships: \n\n");
+                    printf("%d ",m[i][j].shipSize);
+                    
+                    break;
+
+                case PRINT_PROB:
+                    if(!i && !j) 
+                        printf("\n\n Displaying Probabilities: \n\n");
+                    printf("%d ",m[i][j].prob);
+                    
+                    break;
+
+                case PRINT_FIREDUPON:
+                    if(!i && !j) 
+                        printf("\n\n Displaying firedUpOn: \n\n");
+                    if(m[i][j].shipExists){
+                        printf("*%d ",m[i][j].firedUpon);
+                    } else {
+                        printf("%2d ",m[i][j].firedUpon);
+                    }
+                    break;
+
+                case PRINT_SHIP_ID:
+                    if(!i && !j) 
+                        printf("\n\n Displaying Ships ID's: \n\n");
+                    printf("%d ",m[i][j].id);
+                    break;    
+
+            }
+            
         }
         printf("\n");
     }
@@ -264,31 +451,61 @@ void displayProbs(Point m[MATRIX_SIZE][MATRIX_SIZE]){
     printf("-------------------------\n");
 }
 
-void displayMatrix(Point m[MATRIX_SIZE][MATRIX_SIZE]){
-    int i,j;
-    printf("\n\n Displaying Matrix: \n\n");
-    for(i = 0; i < MATRIX_SIZE; i++){
-        for(j = 0; j < MATRIX_SIZE; j++){
-            printf("%d ",m[i][j].shipSize);
+int isEndOfGame(Point m[MATRIX_SIZE][MATRIX_SIZE]){
+    int row,col;
+    int destroyed = TRUE;
+
+    for(row = 0; row < MATRIX_SIZE; row++){
+        for(col = 0; col < MATRIX_SIZE; col++){
+            if(m[row][col].id != 0 && m[row][col].firedUpon == FALSE){
+                destroyed = FALSE;
+                return destroyed;
+            }
         }
-        printf("\n");
     }
 
-    printf("-------------------------\n");
+    return destroyed;
 }
 
-void displayFiredUpOn(Point m[MATRIX_SIZE][MATRIX_SIZE]){
-    int i,j;
-    printf("\n\n Displaying firedUpon: \n\n");
-    for(i = 0; i < MATRIX_SIZE; i++){
-        for(j = 0; j < MATRIX_SIZE; j++){
-            printf("%d ",m[i][j].firedUpon);
+int ifShipDestroyed(Point m[MATRIX_SIZE][MATRIX_SIZE],int id){
+    int row,col;
+    int destroyed = TRUE;
+
+    for(row = 0; row < MATRIX_SIZE; row++){
+        for(col = 0; col < MATRIX_SIZE; col++){
+            if(m[row][col].id == id && m[row][col].firedUpon == FALSE){
+                destroyed = FALSE;
+                return destroyed;
+            }
         }
-        printf("\n");
     }
 
-    printf("-------------------------\n");
+    return destroyed;
+    
 }
+
+int ifSameShip(Point m[MATRIX_SIZE][MATRIX_SIZE],Point p1, Point p2 ){
+    if(m[p1.x][p1.y].id == m[p2.x][p2.y].id 
+        && m[p1.x][p1.y].id != 0){
+        return SUCCESS;
+    }
+
+    return FAIL;
+
+}
+
+void displayFireDir(int fire_dir){
+    if(fire_dir == FIRE_IDLE){
+        printf("fire_dir: FIRE_IDLE\n");
+    } else if(fire_dir == FIRE_ROW){
+        printf("fire_dir: FIRE_ROW\n");
+    } else if(fire_dir == FIRE_COL){
+        printf("fire_dir: FIRE_COL\n");
+    } else {
+        printf("fire_dir: ERROR\n"); 
+    }
+}
+
 
 void outProbResults(Point m[MATRIX_SIZE][MATRIX_SIZE], FILE *fptr,int round ){
     int row,col;
@@ -306,16 +523,22 @@ void outProbResults(Point m[MATRIX_SIZE][MATRIX_SIZE], FILE *fptr,int round ){
 int main(int argc, char *argv[])
 {
     int row,col;
+    int mode;
     Point m[MATRIX_SIZE][MATRIX_SIZE];
-    int remaining_ship_freq[4] = {1,2,2,3};
+    int remaining_ship_freq[4] = {3,2,2,1};
+    srand(time(NULL));
+
 
     /*
 
     Available ships:
-    size 5, freq 1
-    size 4, freq 2
-    size 3, freq 2
     size 2, freq 3
+    size 3, freq 2
+    size 4, freq 2
+    size 5, freq 1
+    
+   
+    
 
         0 1 2 3 4 5 6 7
 
@@ -331,45 +554,111 @@ int main(int argc, char *argv[])
     */
 
     placeShips(m);
-
     int not_destroyed = TRUE;
     int counter = 1;
+    int containsShip = 0;
+    Point target_point;
     FILE *fptr = fopen("results.txt", "w");
+    int ship_counter = 0;
+    Point last_successfull_point;
+    int fire_dir = FIRE_IDLE; 
+    int shipDestroyed = FALSE;
+    Point first_s_point;
+    int fire_counter = 0;
+
+    //implement a circular buffer with all successfull attemps for one ship
+    // so that you can try all the possibilities in while loop by calling fire_findshot
+
+    //game loop    
     while(not_destroyed){
-        printf("\n^^^^^^^^^^^^^^^^^^^  round %d  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^",counter);
-        Point target_point = findBestShot(m,remaining_ship_freq);
+        printf("\n^^^^^^^^^^^^^^^^^^^  round %d  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n",counter);
+
+         //set the mode
+        int mode = (fire_counter > 0 && !shipDestroyed)? MODE_FIRE : MODE_HUNT;
+        printf("target_point: (%d,%d), fired %d \n",target_point.x,target_point.y,target_point.firedUpon);
+        //hunting mode
+        if(mode == MODE_HUNT){
+            fire_counter = 0;
+            printf(" mode: HUNT\n ");
+            target_point = hunt_findShot(m,remaining_ship_freq);
+        } else if(mode == MODE_FIRE){
+            if(fire_counter == 1)
+                first_s_point = last_successfull_point;
+
+            displayFireDir(fire_dir);
+            printf("last_successfull_point: (%d,%d)\n",last_successfull_point.x,last_successfull_point.y);
+            target_point = fire_findShot(m,remaining_ship_freq,last_successfull_point, fire_dir);
+
+            if(target_point.prob == 0){
+                printf("trying with point: (%d,%d)\n",first_s_point.x,first_s_point.y);
+                target_point = fire_findShot(m,remaining_ship_freq,first_s_point, fire_dir);
+            }
+    
+        }
+             
         printf("target_point: (%d,%d)\n",target_point.x,target_point.y);
-        int containsShip = fire(target_point,m);
+        containsShip = fire(target_point,m);   
+
+        //if shot is succesfull, update point
+        //update position in which  
+        if(containsShip){
+            fire_counter++;
+            //check if it is the second successfull shot
+            if(fire_counter > 1){
+                if(target_point.x == last_successfull_point.x && ifSameShip(m,target_point,last_successfull_point)){
+                    fire_dir = FIRE_ROW;
+                } else if(target_point.y == last_successfull_point.y && ifSameShip(m,target_point,last_successfull_point)){
+                    fire_dir = FIRE_COL;
+                } else {
+                    fire_dir = FIRE_IDLE;
+                }
+            } else {
+                fire_dir = FIRE_IDLE;
+            }
+            last_successfull_point = target_point;
+
+            //check if ship is destroyed
+            shipDestroyed = ifShipDestroyed(m,target_point.id);
+
+            if(shipDestroyed){
+
+                not_destroyed = isEndOfGame(m)? FALSE : TRUE;
+                int temp_size = target_point.shipSize ;
+                remaining_ship_freq[temp_size-2] = remaining_ship_freq[temp_size-2] - 1;
+                printf("new frequency of size %d: %d\n",temp_size,remaining_ship_freq[temp_size-2]);
+            } 
+
+        }
+
+        printf("\nis ship destroyed: %d\n",shipDestroyed);
+    
+
         printf("ship: %d",containsShip);
-        displayProbs(m);
+        displayMatrix(m,PRINT_FIREDUPON);
         outProbResults(m,fptr,counter);
 
-        if(counter == 6){
-            break;
-        }
+        if(counter == 64){
+            printf(" \n\n *******65th shot game ended\n\n\n");
+                break;
+         }
 
         counter++;
     }
 
     fclose(fptr);
+    printf("\nrounds ended\n");
+    printf("\nfinished on round: %d\n\n",counter);
+    if(isEndOfGame)
+        printf("\n****** VICTORY ******\n");
 
-    Point target_point = findBestShot(m,remaining_ship_freq);
-    printf("target_point: (%d,%d)\n",target_point.x,target_point.y);
-    int containsShip = fire(target_point,m);
-    printf("ship: %d",containsShip);
-    displayProbs(m);
-    displayMatrix(m);
-    displayFiredUpOn(m);
+    
+    displayMatrix(m,PRINT_PROB);
+    displayMatrix(m,PRINT_SHIPS);
+    displayMatrix(m,PRINT_FIREDUPON);
 
 
 
     return;
 }
-
-
-
-
-
-
 
 
--- algo.h
+++ algo.h
@@ -6,6 +6,20 @@
 #define SUCCESS 1
 #define FAIL 0
 
+#define MODE_FIRE 99
+#define MODE_HUNT 100
+
+#define ARR_FIRE_SIZE 4
+
+#define PRINT_PROB -1
+#define PRINT_SHIP_ID -2
+#define PRINT_FIREDUPON -3
+#define PRINT_SHIPS -4 
+
+#define FIRE_ROW -10
+#define FIRE_COL -11
+#define FIRE_IDLE -12
+
 typedef int bool;
 #define TRUE 1
 #define FALSE 0
